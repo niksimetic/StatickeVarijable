979e43c81dd6b6ed7194b0872f2cb6cfd8ab9ecb
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-10-06 11:31:12 +0200

    Add files via upload

---
+++ nikSimeticStatickeVarijable/nikSimeticStatickeVarijable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimeticStatickeVarijable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa("Nik","Simetic");
+            Console.WriteLine(x.ToString());
+            PrvaKlasa y = new PrvaKlasa("Matija","Tomc");
+            Console.WriteLine(y.ToString());
+            PrvaKlasa z = new PrvaKlasa();
+            Console.WriteLine(z.ToString());
+
+            z.Ime = "Lenco";
+            z.Prezime = "Krek";
+            Console.WriteLine(z.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ nikSimeticStatickeVarijable/nikSimeticStatickeVarijable/PrvaKlasa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimeticStatickeVarijable
+{
+    class PrvaKlasa
+    {
+        private static int counter;
+        private string ime;
+        private string prezime;
+        public PrvaKlasa()
+        {
+            counter++;
+         
+
+        }
+        public PrvaKlasa(string ime, string prezime)
+        {
+            counter++;
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+        public int Counter { get => counter; }
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+       
+        public override string ToString()
+        {
+            string ispis = "Redni broj: " + counter+", "+ ime+" "+prezime;
+            
+            return ispis;
+        }
+    }
+}
